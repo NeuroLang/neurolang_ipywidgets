5373159a453ed126a1542b7dc07c1837fe02d72f
Hande Gözükan <hande.gozukan@inria.fr>
2020-05-29 12:41:50 +0200

    add layout to example

--- examples/papaya.py
+++ examples/papaya.py
@@ -1,5 +1,7 @@
 from neurolang_ipywidgets import NlPapayaViewer
+from ipywidgets import Layout
 
-NlPapayaViewer()
+nl = NlPapayaViewer(layout=Layout(width="800px", height="650px", border="1px solid black"))
+nl
 
 
