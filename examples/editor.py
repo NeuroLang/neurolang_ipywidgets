f3d89ae25fe65cc8b682c3e8de4e93e4cfd10ae7
Romain PRIMET <romain.primet@inria.fr>
2020-06-09 17:00:35 +0200

    add note about line numbering for marks

--- examples/editor.py
+++ examples/editor.py
@@ -11,7 +11,6 @@ def foo()
     bar.baz()
 """
 
+# Note that CodeMirror has a 0-index convention for line numbers
 n.marks = [{"line": 0, "text": "parse error: \n    Expected something, got something else..."},
           {"line": 2, "text": "missing colon"}]
-
-
