dcc118f77f1b38735f0d01157c5880c0cfc19b18
Hande Gözükan <hande.gozukan@inria.fr>
2020-07-01 13:15:33 +0200

    update notebook to demonstrate colorbar when images added removed

--- examples/papaya.py
+++ examples/papaya.py
@@ -7,7 +7,7 @@ import nibabel as nib
 
 # ## Initialize papaya viewer
 
-nl = NlPapayaViewer(layout=Layout(width="700px", height="600px"))
+nl = NlPapayaViewer(layout=Layout(width="800px", height="700px"))
 nl
 
 # ## Display Color Bar
@@ -27,19 +27,19 @@ nl.show_image_colorbar_at_index(0)
 # ## Add Labels
 
 # set config
-config = dict(min=0, max=10, lut="Green Overlay")
+config_green = dict(min=0, max=10, lut="Green Overlay")
+config_blue = dict(min=0, max=10, lut="Blue Overlay")
+config_gold = dict(min=0, max=10, lut="Gold")
+config_fire = dict(min=0, max=10, lut="Fire")
 
-papaya_image1 = PapayaNiftiImage(nib.load("img/L_G_front_sup.nii"), config)
+papaya_image1 = PapayaNiftiImage(nib.load("img/L_G_front_sup.nii"), config_green)
 nl.add([papaya_image1])
 
-# set config
-config = dict(min=0, max=10, lut="Blue Overlay")
-
-papaya_image2 = PapayaNiftiImage(nib.load("img/L_S_circular_insula_sup.nii"), config)
+papaya_image2 = PapayaNiftiImage(nib.load("img/L_S_circular_insula_sup.nii"), config_blue)
 nl.add([papaya_image2])
 
-papaya_image3 = PapayaNiftiImage(nib.load("img/R_S_circular_insula_sup.nii"), config)
-papaya_image4 = PapayaNiftiImage(nib.load("img/R_G_front_inf-Opercular.nii"), config)
+papaya_image3 = PapayaNiftiImage(nib.load("img/R_S_circular_insula_sup.nii"), config_gold)
+papaya_image4 = PapayaNiftiImage(nib.load("img/R_G_front_inf-Opercular.nii"), config_fire)
 nl.add([papaya_image3, papaya_image4])
 
 # ## Remove Label
