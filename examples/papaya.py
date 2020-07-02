b5541241fc858438598257089dd88b2d48781b7c
Hande Gözükan <hande.gozukan@inria.fr>
2020-07-02 16:36:14 +0200

    update example to use aumated lut selection for custom labels

--- examples/papaya.py
+++ examples/papaya.py
@@ -7,7 +7,7 @@ import nibabel as nib
 
 # ## Initialize papaya viewer
 
-nl = NlPapayaViewer(layout=Layout(width="800px", height="700px"))
+nl = NlPapayaViewer(layout=Layout(width="500px", height="470px"))
 nl
 
 # ## Add Overlay
@@ -18,7 +18,7 @@ nl.add([papaya_image_overlay])
 # ## Set Color Bar by Image Index
 
 # 0 is index for atlas
-nl.show_image_colorbar_at_index(0)
+nl.show_image_colorbar_at_index(2)
 
 # ## Hide/Show Color Bar
 
@@ -28,20 +28,14 @@ nl.colorbar = True
 
 # ## Add Labels
 
-# set config
-config_green = dict(min=0, max=10, lut="Green Overlay")
-config_blue = dict(min=0, max=10, lut="Blue Overlay")
-config_gold = dict(min=0, max=10, lut="Gold")
-config_fire = dict(min=0, max=10, lut="Fire")
-
-papaya_image1 = PapayaNiftiImage(nib.load("img/L_G_front_sup.nii"), config_green)
+papaya_image1 = PapayaNiftiImage(nib.load("img/L_G_front_sup.nii"))
 nl.add([papaya_image1])
 
-papaya_image2 = PapayaNiftiImage(nib.load("img/L_S_circular_insula_sup.nii"), config_blue)
+papaya_image2 = PapayaNiftiImage(nib.load("img/L_S_circular_insula_sup.nii"))
 nl.add([papaya_image2])
 
-papaya_image3 = PapayaNiftiImage(nib.load("img/R_S_circular_insula_sup.nii"), config_gold)
-papaya_image4 = PapayaNiftiImage(nib.load("img/R_G_front_inf-Opercular.nii"), config_fire)
+papaya_image3 = PapayaNiftiImage(nib.load("img/R_S_circular_insula_sup.nii"))
+papaya_image4 = PapayaNiftiImage(nib.load("img/R_G_front_inf-Opercular.nii"))
 nl.add([papaya_image3, papaya_image4])
 
 # ## Remove Label
