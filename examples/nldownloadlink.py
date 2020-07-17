47deddf0cdaae30b54c3bb10913ce88bc3d86c2f
Hande Gözükan <hande.gozukan@inria.fr>
2020-07-17 14:30:16 +0200

    update example to add download link for image file

--- examples/nldownloadlink.py
+++ examples/nldownloadlink.py
@@ -12,11 +12,17 @@
 #     name: python3
 # ---
 
-from neurolang_ipywidgets import NlDownloadLink
+# +
+from neurolang_ipywidgets import NlDownloadLink, PapayaNiftiImage
+
+import nibabel as nib
 import numpy as np
 import pandas as pd
+# -
 
-# ## Create dataframe
+# ## Download CSV file
+
+# ### Create dataframe
 
 # +
 # Set the seed for a reproducible sample
@@ -26,12 +32,14 @@ df = pd.DataFrame(np.random.randn(50, 3), columns=list('ABC'))
 df.head()
 # -
 
-# ### Download link
+df_csv = df.to_csv(index=False).encode()
 
-download_link = NlDownloadLink(filename="random.csv", content=df.to_csv(index=False))
+# #### Download link
+
+download_link = NlDownloadLink(filename="random.csv", content=df_csv)
 download_link
 
-# ### Lazy download link
+# #### Lazy download link
 
 # +
 # When files are big, this helps generate csv file only when link is clicked
@@ -39,10 +47,19 @@ download_link_lazy = NlDownloadLink(filename="random_lazy.csv")
 
 
 def clicked(event):
-    download_link_lazy.content = df.to_csv()
+    download_link_lazy.content = df_csv
 
 download_link_lazy.on_click(clicked)
 download_link_lazy
 # -
+# # Download Nifti image
+
+
+papaya_image = PapayaNiftiImage(nib.load("img/L_G_front_sup.nii"))
+
+image_link = NlDownloadLink(filename="image.nii", content=papaya_image.image.to_bytes())
+image_link
+
+papaya_image_test = PapayaNiftiImage(nib.load("img/image.nii"))
 
 
