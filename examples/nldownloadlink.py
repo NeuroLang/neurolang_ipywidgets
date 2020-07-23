f34205cc9eef0dff405ce5fd1ddf9beb5d43fc1e
Hande Gözükan <hande.gozukan@inria.fr>
2020-07-23 16:00:40 +0200

    update download example with compressed files

--- examples/nldownloadlink.py
+++ examples/nldownloadlink.py
@@ -15,6 +15,8 @@
 # +
 from neurolang_ipywidgets import NlDownloadLink, PapayaNiftiImage
 
+import gzip
+from ipywidgets import Layout
 import nibabel as nib
 import numpy as np
 import pandas as pd
@@ -26,7 +28,7 @@ import pandas as pd
 
 # +
 # Set the seed for a reproducible sample
-np.random.seed(0)  
+np.random.seed(0)
 
 df = pd.DataFrame(np.random.randn(50, 3), columns=list('ABC'))
 df.head()
@@ -34,41 +36,72 @@ df.head()
 
 df_csv = df.to_csv(index=False).encode()
 
-# #### Download link
+layout = Layout(width="30px")
 
-dl_1 = NlDownloadLink(filename="random.csv", content=df_csv)
-dl_1 
+# ### Download link
 
-# #### Lazy download link
+dl_1 = NlDownloadLink(filename="random.csv", content=df_csv, layout=layout)
+dl_1
+
+# ### Lazy download link
 
 # +
 # When files are big, this helps generate csv file only when link is clicked
-dl_2 = NlDownloadLink(filename="random_lazy.csv")
+dl_2 = NlDownloadLink(filename="random_lazy.csv", layout=layout)
 
 
 def clicked(event):
     dl_2.content = df_csv
 
+
 dl_2.on_click(clicked)
-dl_2 
+dl_2
 # -
-# #### Named link with description and tooltip
+# ### Link for compressed data file
 
-dl_3 = NlDownloadLink(filename="random.csv", content=df_csv, 
-                      tooltip="Download random.csv", description= "random.csv")
+# +
+compressed = gzip.compress(df_csv)
+
+dl_3 = NlDownloadLink(filename="random_compressed.csv.gz", content=compressed,
+                      mimetyle="application/gz", layout=layout)
 dl_3
+# -
 
-# #### Disabled link
+# ### Named link with description and tooltip
 
-dl_4 = NlDownloadLink(filename="random.csv", tooltip="Download disabled", disabled=True)
+dl_4 = NlDownloadLink(filename="random.csv", content=df_csv,
+                      tooltip="Download random.csv", description="random.csv")
 dl_4
 
-# # Download Nifti image
+# ### Disabled link
 
+dl_5 = NlDownloadLink(filename="random.csv",
+                      tooltip="Download disabled", disabled=True, layout=layout)
+dl_5
+
+# ## Download Nifti image
+
+
+# ### Load image file
 
 papaya_image = PapayaNiftiImage(nib.load("img/L_G_front_sup.nii"))
 
-image_link = NlDownloadLink(filename="image.nii", content=papaya_image.to_bytes())
-image_link
+# ### Download image link
 
-papaya_image_test = PapayaNiftiImage(nib.load("img/image.nii"))
+image_link_1 = NlDownloadLink(
+    filename="image.nii", content=papaya_image.to_bytes(), layout=layout)
+image_link_1
+
+# this will give error if image.nii file is not downloaded in previous cell
+papaya_image_1_test = PapayaNiftiImage(nib.load("img/image.nii"))
+
+# ### Link for compressed image
+
+compressed_image = gzip.compress(papaya_image.to_bytes())
+image_link_2 = NlDownloadLink(
+    filename="image.nii.gz", content=compressed_image, layout=layout)
+image_link_2
+
+papaya_image_2_test = PapayaNiftiImage(nib.load("img/image.nii.gz"))
+
+
